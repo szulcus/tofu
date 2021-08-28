18c9d8b051607fd0e334ab12e51335702f8b09a3
jakub104 <jakubzlezena@wp.pl>
2021-08-28 20:36:19 +0200

    deploy

---
+++ precache-manifest.339abe3931bbc60f0e5fd75159efcc75.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866d5bce7cca4e782aed",
+    "url": "/tofu/css/app.e01dbb1c.css"
+  },
+  {
+    "revision": "746c2f665af6e2093a4b",
+    "url": "/tofu/css/chunk-1232afc8.ef900514.css"
+  },
+  {
+    "revision": "ff39c85334ea3fb122dc",
+    "url": "/tofu/css/chunk-3c78a07a.9bfeac07.css"
+  },
+  {
+    "revision": "daa10124b5865a469c4c",
+    "url": "/tofu/css/chunk-79b9e329.109bef4f.css"
+  },
+  {
+    "revision": "ca5fae1466c613505a67",
+    "url": "/tofu/css/chunk-8113c880.3a7036b7.css"
+  },
+  {
+    "revision": "432b14cb7e760644f6bd",
+    "url": "/tofu/css/chunk-89b66538.1628fe8e.css"
+  },
+  {
+    "revision": "16920918cca742a42597",
+    "url": "/tofu/css/chunk-ce2d957c.8f2751cd.css"
+  },
+  {
+    "revision": "1e69698ceabcc978f8c8",
+    "url": "/tofu/css/chunk-e402a64e.04c0fc1d.css"
+  },
+  {
+    "revision": "8ac7a1703ba337a724be",
+    "url": "/tofu/css/chunk-fd5e786a.9e43710e.css"
+  },
+  {
+    "revision": "08c7865d7e116fc450a0653ccb38e0fb",
+    "url": "/tofu/img/logo.08c7865d.svg"
+  },
+  {
+    "revision": "232457d0c3f8749275d7d74ae1d2c5a4",
+    "url": "/tofu/img/logotype.232457d0.svg"
+  },
+  {
+    "revision": "5254587480471ca5b10b155ebe1928ee",
+    "url": "/tofu/index.html"
+  },
+  {
+    "revision": "866d5bce7cca4e782aed",
+    "url": "/tofu/js/app.1b7883eb.js"
+  },
+  {
+    "revision": "746c2f665af6e2093a4b",
+    "url": "/tofu/js/chunk-1232afc8.819b3a11.js"
+  },
+  {
+    "revision": "ff39c85334ea3fb122dc",
+    "url": "/tofu/js/chunk-3c78a07a.3b0adf0c.js"
+  },
+  {
+    "revision": "daa10124b5865a469c4c",
+    "url": "/tofu/js/chunk-79b9e329.ee1d0ce9.js"
+  },
+  {
+    "revision": "ca5fae1466c613505a67",
+    "url": "/tofu/js/chunk-8113c880.861a62b1.js"
+  },
+  {
+    "revision": "432b14cb7e760644f6bd",
+    "url": "/tofu/js/chunk-89b66538.6dc22d12.js"
+  },
+  {
+    "revision": "16920918cca742a42597",
+    "url": "/tofu/js/chunk-ce2d957c.8e9624a8.js"
+  },
+  {
+    "revision": "1e69698ceabcc978f8c8",
+    "url": "/tofu/js/chunk-e402a64e.5243226d.js"
+  },
+  {
+    "revision": "8ac7a1703ba337a724be",
+    "url": "/tofu/js/chunk-fd5e786a.a0a0b172.js"
+  },
+  {
+    "revision": "96f811b44eee5b845bb6",
+    "url": "/tofu/js/chunk-vendors.967e4cec.js"
+  },
+  {
+    "revision": "feae235f42935efe63099b9efd0e685c",
+    "url": "/tofu/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tofu/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tofu/precache-manifest.339abe3931bbc60f0e5fd75159efcc75.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "tofu"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
